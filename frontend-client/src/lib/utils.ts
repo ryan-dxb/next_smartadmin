fa3ed810b1a77ff5b1c7d6b370bcea9a06c14d4d
Ryan Pereira <ryan.dxb92@gmail.com>
2023-05-29 08:48:24 +0400

    Made Changes to Auth Files and redux store

--- frontend-client/src/lib/utils.ts
+++ frontend-client/src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge";
 export function cn(...input: ClassValue[]) {
   return twMerge(clsx(...input));
 }
+
+export function chatHrefContructor(id1: string, id2: string) {
+  const sortedId = [id1, id2].sort();
+
+  return `${sortedId[0]}--${sortedId[1]}`;
+}
